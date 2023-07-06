c8e31ff22816f9cfc186438d574182600284d587
SamZakharov <semen.zaharovv@gmail.com>
2023-07-06 20:13:07 +0300

    2

--- js/scripts.js
+++ js/scripts.js
@@ -1,9 +1,9 @@
 var menuBtn = document.querySelector('.menu-btn');
-var nav = document.querySelector('.head-wrapper');
-var lineOne = document.querySelector('.head-wrapper .menu-btn .line--1');
-var lineTwo = document.querySelector('.head-wrapper .menu-btn .line--2');
-var lineThree = document.querySelector('.head-wrapper .menu-btn .line--3');
-var link = document.querySelector('.head-wrapper .nav-links');
+var nav = document.querySelector('nav');
+var lineOne = document.querySelector('nav .menu-btn .line--1');
+var lineTwo = document.querySelector('nav .menu-btn .line--2');
+var lineThree = document.querySelector('nav .menu-btn .line--3');
+var link = document.querySelector('nav .nav-links');
 menuBtn.addEventListener('click', () => {
     nav.classList.toggle('nav-open');
     lineOne.classList.toggle('line-cross');
